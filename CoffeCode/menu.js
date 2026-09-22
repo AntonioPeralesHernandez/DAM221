f58a923813d0538ab7077e697b5b1e187a4ba026
AntonioPeralesHernandez <123046676@upq.edu.mx>
2026-09-22 00:52:16 -0600

    Integración Códigos Menú Principal

--- CoffeCode/menu.js
+++ CoffeCode/menu.js
@@ -1,5 +1,5 @@
 const readline = require("readline");
-const {spawnSync} = require("child_process");
+const {spawn} = require("child_process");
 
 function mostrarMenu() {
     console.log(`
@@ -12,37 +12,41 @@ function mostrarMenu() {
         4.Salir
         =================================
     `);
+
     const rl = readline.createInterface({
         input: process.stdin,
         output: process.stdout
     });
 
     rl.question('Selecciona una opción (1-4): ', (opcion) => {
-    rl.close();
-    switch (opcion) {
-        case "1":
-            spawnSync('node', ['caja.js'], { stdio: 'inherit' });
-            mostrarMenu();
-            break;
-        case "2":
-            spawnSync('node', ['cocina.js'], { stdio: 'inherit' });
-            mostrarMenu();
-            break;
-        case "3":
-            spawnSync('node', ['clientes.js'], { stdio: 'inherit' });
-            mostrarMenu();
-            break;
-        case "4":
-            console.log("Saliendo del programa...");
-            break;
-            
-        default:
-            console.log("Opción inválida. Intenta de nuevo.");
-            mostrarMenu();
-            break;
-    
-    }
-});
-}
-mostrarMenu();
+        rl.close();
 
+        switch (opcion) {
+            case "1":
+                spawn('node', ['caja.js'], {stdio: 'inherit'})
+                    .on('close', mostrarMenu);
+                break;
+
+            case "2":
+                spawn('node', ['cocina.js'], {stdio: 'inherit'})
+                    .on('close', mostrarMenu);
+                break;
+
+            case "3":
+                spawn('node', ['clientes.js'], {stdio: 'inherit'})
+                    .on('close', mostrarMenu);
+                break;
+
+            case "4":
+                console.log("Saliendo del programa...");
+                break;
+
+            default:
+                console.log("Opción inválida. Intenta de nuevo.");
+                mostrarMenu();
+                break;
+        }
+    });
+}
+
+mostrarMenu();
