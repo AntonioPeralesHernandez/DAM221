c7d6e1c48a664f840551499c6487a394418db133
AntonioPeralesHernandez <123046676@upq.edu.mx>
2026-09-22 00:53:39 -0600

    Avance 2 Completo Cocina(Antonio Perales)

--- CoffeCode/cocina.js
+++ CoffeCode/cocina.js
@@ -1,9 +1,12 @@
 let productos =[
     {id:1, nombre: "Café sin azucar", precio: 40, categoria: "bebida"},
-    {id:2, nombre: "Café Italiano", precio: 45, categoria:"bebida"},
+    {id:2, nombre: "Café Italiano", precio: 60, categoria:"bebida"},
     {id:3, nombre: "Pan de muertos", precio: 15, categoria:"postre"},
+    {id:4, nombre: "Pastel chocolate", precio: 50, categoria:"postre"},
+    {id:5, nombre: "Café con leche ", precio: 42, categoria:"bebida"},
 ];
-function agregar(nombre, precio){
+
+function agregar(nombre, precio, categoria){
     productos.push({
         id: productos.length + 1,
         nombre: nombre,
@@ -11,74 +14,133 @@ function agregar(nombre, precio){
         categoria: categoria
     });
 }
-function editar(id, nombre, precio){
+
+function editar(id, nombre, precio, categoria){
     let producto = productos.find(p => p.id === id);
 
     if(producto){
         producto.nombre = nombre;
         producto.precio = precio;
         producto.categoria = categoria;
-
     }
 }
+
 function eliminar(id){
     productos = productos.filter(p => p.id !== id);
-
 }
+
 function listar(){
     console.log("\n---PRODUCTOS---");
-
     productos.forEach(p => {
         console.log(
-    `ID: ${p.id} | Nombre: ${p.nombre} | Precio: ${p.precio} | Categoria: ${p.categoria}`
+            `ID: ${p.id} | Nombre: ${p.nombre} | Precio: ${p.precio} | Categoria: ${p.categoria}`
         );
     });
 }
 
+function buscarProductos(tipo){
 
-const readline = require("readline").createInterface({
-    input: process.stdin, output: process.stdout
-});
-readline.question(`
-1. Agregar producto
-2. Editar producto
-3. Eliminar producto
-4. Listar productos
-5. Buscar producto
-6. Salir
+    if(tipo === "1"){
+        let resultado = productos.filter(p => p.precio <= 45);
+        console.log("\n---PRODUCTOS BARATITOS---");
+        resultado.forEach(p => console.log(
+            `ID: ${p.id} | Nombre: ${p.nombre} | Precio: ${p.precio} | Categoria: ${p.categoria}`
+        ));
+    }
 
-Selecciona una opción: `, opcion =>{
-    if (opcion === "1"){
-        readline.question("Nombre: ", nombre =>
-        readline.question("Precio: ", precio => 
-        readline.question("Categoria: ", categoria => {
-            agregar(nombre, Number(precio), categoria);
+    else if(tipo === "2"){
+        let resultado = productos.filter(p => p.precio > 45);
+        console.log("\n---PRODUCTOS CARISIMOS DE PARÍS---");
+        resultado.forEach(p => console.log(
+            `ID: ${p.id} | Nombre: ${p.nombre} | Precio: ${p.precio} | Categoria: ${p.categoria}`
+        ));
+    }
+
+    else if(tipo === "3"){
+        let resultado = productos.find(p => p.categoria === "bebida");
+        console.log("\n---BEBIDAS PA' LA SED---");
+        console.log(resultado);
+    }
+
+    else if(tipo === "4"){
+        let resultado = productos.find(p => p.categoria === "postre");
+        console.log("\n---POSTRESITOS---");
+        console.log(resultado);
+    }
+}
+
+    const readline = require("readline");
+    function mostrarMenu(){
+    const rl = readline.createInterface({
+        input: process.stdin, 
+        output: process.stdout
+    });
+
+    rl.question(`
+        1. Agregar producto
+        2. Editar producto
+        3. Eliminar producto
+        4. Listar productos
+        5. Buscar producto
+        6. Salir
+        Selecciona una opción: `, opcion =>{
+
+        if (opcion === "1"){
+            rl.question("Nombre: ", nombre =>
+            rl.question("Precio: ", precio => 
+            rl.question("Categoria: ", categoria => {
+
+                agregar(nombre, Number(precio), categoria);
+                listar();
+                rl.close();
+                mostrarMenu();
+            })));
+        }
+
+        else if (opcion === "2"){
+            rl.question("ID: ", id => 
+            rl.question("Nombre: ", nombre =>
+            rl.question("Precio: ", precio => 
+            rl.question("Categoria: ", categoria => {
+
+                editar(Number(id), nombre, Number(precio), categoria);
+                listar();
+                rl.close();
+                mostrarMenu();
+            }))));
+        }
+
+        else if (opcion === "3"){
+            rl.question("ID: ", id => {
+                eliminar(Number(id));
+                listar();
+                rl.close();
+                mostrarMenu();
+            });
+        }
+
+        else if (opcion === "4"){
             listar();
-            readline.close();
-        }));
-}
-else if (opcion === "2"){
-    readline.question("ID: ", id => 
-readline.question("Nombre: ", nombre =>
-readline.question("Precio: ", precio => {
-    editar(Number(id), nombre, Number(precio));
-    listar();
-    readline.close();
-})));
-}
-else if (opcion === "3"){
-    readline.question("ID: ", id => {
-        eliminar(Number(id));
-        listar();
-        readline.close();
+            rl.close();
+            mostrarMenu();
+        }
+
+        else if (opcion === "5"){
+            rl.question(`
+            1. Productos baratos
+            2. Productos caros
+            3. Bebidas
+            4. Postres
+            Selecciona qué quieres buscar: `, tipo => {
+            buscarProductos(tipo);
+            rl.close();
+            mostrarMenu();
+            });
+        }
+
+        else if (opcion === "6"){
+            rl.close();
+        }
     });
 }
-else if (opcion === "4"){
-    listar();
-    readline.close();
-}
-else if (opcion === "5"){
-    readline.close();
-}
-});
-
+mostrarMenu();
