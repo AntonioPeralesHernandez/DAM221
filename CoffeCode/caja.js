8eb2a86901f977b1d0cafb96b05737541510f6da
AntonioPeralesHernandez <123046676@upq.edu.mx>
2026-09-24 23:36:20 -0600

    Avance Caja 3 Completo (Diego Carreño)

--- CoffeCode/caja.js
+++ CoffeCode/caja.js
@@ -1,175 +1,144 @@
-const readline = require('readline');
-
 let pedidos = [];
-let totalAcumulado = 0;
 
-let productos = [
-    { id: 1, nombre: "Cafe americano", precio: 40 },
-    { id: 2, nombre: "Cafe con leche", precio: 45 },
-    { id: 3, nombre: "Pan de Muerto ", precio: 15 }
-];
+const IVA = 0.21;
 
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
-function mostrarProductos() {
-    console.log("\nProductos disponibles \n elige tu opcion:");
-    console.log("1. Agregar pedido");
-    console.log("2. Mostrar pedidos");
-    console.log("3. Mostrar total acumulado");
-    console.log("4. Eliminar pedido");
-    console.log("5. Modificar pedido");
-    console.log("6. Calcular total con IVA ( ni modos )");
-    console.log("7. Salir");
-    productos.forEach((producto) => {
-        console.log(`${producto.id}. ${producto.nombre} - $${producto.precio}`);
-    });
-}
-
-function agregarPedido(nombre, precio) {
-    let pedido = { nombre: nombre, precio: precio };
+function agregarPedido(pedido) {
     pedidos.push(pedido);
-    totalAcumulado += precio;
-    console.log(`\n Pedido agregado correctamente: ${nombre} - $${precio}`);
+
+    console.log(
+        `Pedido #${pedido.idPedido} registrado en Caja. `
+    );
 }
 
 function mostrarPedidos() {
-    console.log("\nPedidos realizados:");
+    console.log(" \n Pedidos en Caja:");
     if (pedidos.length === 0) {
-        console.log("No hay pedidos realizados.");
+        console.log("No hay pedidos registrados por el momentoooo .");
         return;
     }
+
     pedidos.forEach((pedido, index) => {
-        console.log(`${index + 1}. ${pedido.nombre} - $${pedido.precio}`);
+        console.log(
+            `${index + 1}. Pedido #${pedido.idPedido} | ${pedido.producto } | $${pedido.subtotal} | estado:${pedido.estado}`
+        );
     });
 }
 
-function mostrarTotal() {
-    console.log(`Total acumulado: $${totalAcumulado}`);
+function mostrarTotalAcumulado(){
+    const totalAcomulado = pedidos.reduce(
+        (acumulador, pedido) => {
+            const {subtotal, estado} = pedido;
+            if(estado !== "cancelado"){
+                return acumulador + subtotal;
+            }
+            return acumulador;
+        },
+        0
+    );
+    console.log(
+        `Total acumulado : $${totalAcomulado.toFixed(2)}`
+    );
+}
+function calcularTotal(){
+    const subtotal = pedidos.reduce(
+        (acumulador, pedido) => {
+            const {subtotal, estado} = pedido;
+            if( estado !== "cancelado"){
+                return acumulador + subtotal;
+            }
+            return acumulador;
+        },
+        0
+    );
+    const iva = subtotal * IVA;
+    const total = subtotal + iva;
+
+    console.log("\n RESUMEN DE LA CAJITA");
+    console.log(
+        `Subtotal: $${subtotal.toFixed(2)}`
+    );
+    console.log(
+        `IVA (21%): $${iva.toFixed(2)}`
+    );
+    console.log(
+        `Total : $${total.toFixed(2)}`
+    );
 }
 
-function calcularTotal() {
-    let subtotal = pedidos.reduce((acumulador, pedido) => {
-        const { precio } = pedido;
-        return acumulador + precio;
-    }, 0);
-
-    let iva = subtotal * 0.20;
-    let totalConIva = subtotal + iva;
-    console.log("\nRESUMEN DE PEDIDOS");
-    console.log(`Subtotal: $${subtotal}`);
-    console.log(`IVA (20%): $${iva}`);
-    console.log(`Total: $${totalConIva}`);
-}
-
-function eliminarPedido(indice) {
-    if (indice >= 0 && indice < pedidos.length) {
-        let pedidoEliminado = pedidos.splice(indice, 1)[0];
-        totalAcumulado -= pedidoEliminado.precio;
-        console.log(`\n Pedido eliminado correctamente: ${pedidoEliminado.nombre} - $${pedidoEliminado.precio}`
+function notificarPedidoListo(callback){
+    pedidos
+    .filter(pedido => pedido.estado === "listo")
+    .forEach(pedido => {
+        callback(
+            `Pedido #${pedido.idPedido} esta ready bro`
         );
-    } else {
-        console.log("Índice de pedido inválido.");
-    }
+    });
 }
 
-function modificarPedido(indice, nuevoNombre, nuevoPrecio) {
-    if (indice >= 0 && indice < pedidos.length) {
-        let pedidoModificado = pedidos[indice];
-        totalAcumulado -= pedidoModificado.precio;
-        pedidoModificado.nombre = nuevoNombre;
-        pedidoModificado.precio = nuevoPrecio;
-        totalAcumulado += nuevoPrecio;
-        console.log(`\n Pedido modificado correctamente: ${nuevoNombre} - $${nuevoPrecio}`
+function notificarPedidoCancelado(callback){
+    pedidos
+    .filter(pedido => pedido.estado === "cancelado")
+    .forEach(pedido => {
+        callback(
+            `Pedido #${pedido.idPedido} fue cancelado rey motivo: ${pedido.motivoCancelacion}`
         );
-    } else {
-        console.log("Índice de pedido inválido.");
-    }
+    });
 }
 
-function mostrarMenu() {
-    console.log("\n--- SISTEMA DE PEDIDOS ---");
-    console.log("1. Agregar pedido");
-    console.log("2. Mostrar pedidos");
-    console.log("3. Mostrar total acumulado");
-    console.log("4. Eliminar pedido");
-    console.log("5. Modificar pedido");
-    console.log("6. Calcular total con IVA ( ni modos )");
-    console.log("7. Salir");
-}
+function menuCaja(rl, menuPrincipal){
 
-function inicializar() {
-    mostrarProductos();
-    rl.question("HOLAAAA puedes porfas poner una opcion del 1 al 7 : ", (opcion) => {
-        switch (opcion.trim()) {
-            case "1":
-                mostrarProductos();
-                rl.question("ingresa el nombre del producto: ", (nombre) => {
-                    let prodEncontrado = productos.find(p => p.nombre.toLowerCase() === nombre.toLowerCase());
-                    if (prodEncontrado) {
-                        agregarPedido(prodEncontrado.nombre, prodEncontrado.precio);
-                    } else {
-                        console.log("Producto no encontrado.");
-                    }
-                    inicializar();
-                });
-                break;
-                case "2":
-                mostrarPedidos();
-                inicializar();
-                break;
-                case "3":
-                mostrarTotal();
-                inicializar();
-                break;
-            case "4":
-                mostrarPedidos();
-                if (pedidos.length > 0) {
-                    rl.question("Ingrese el número del pedido a eliminar: ", (num) => {
-                        eliminarPedido(parseInt(num) - 1);
-                        inicializar();
-                    });
-                } else {    
-                    inicializar();
-                }
-                break;
-                case "5":
-                mostrarPedidos();
-                if (pedidos.length > 0) {
-                    rl.question("Ingrese el número del pedido a modificar: ", (num) => {
-                        let indice = parseInt(num) - 1;
-                        if (indice >= 0 && indice < pedidos.length) {
-                            rl.question("Ingrese el nuevo nombre del producto: ", (nuevoNombre) => {
-                                rl.question("Ingrese el nuevo precio del producto: ", (nuevoPrecio) => {
-                                    modificarPedido(indice, nuevoNombre, parseFloat(nuevoPrecio));
-                                    inicializar();
-                                });
-                            });
-                        } else {
-                            console.log("Índice de pedido inválido.");
-                            inicializar();
-                        }   
-                    });
-                } else {
-                    inicializar();
-                }
-                break;
-            case "6":
-                calcularTotal();
-                inicializar();
-                break;
-            case "7":
-                console.log("Saliendo del sistema, adiooooossss ");
-                rl.close();
-                break;
-            default:
-                console.log("Opción inválida. Intente nuevamente.");
-                inicializar();
-                break;
+    console.log("\n CAJA COFFEE");
+    console.log("1. mostrar los pedidos ");
+    console.log("2. mostrar total acumulado");
+    console.log("3. calcular total con iva");
+    console.log("4. mostrar pedidos listos");
+    console.log("5. mostrar pedidos cancelados");
+    console.log("6. regresar");
+
+    rl.question("\nSelecciona un opcion: ", opcion => {
+        if(opcion === "1"){
+            mostrarPedidos();
+            menuCaja(rl, menuPrincipal);
+        }
+        else if(opcion === "2"){
+            mostrarTotalAcumulado();
+            menuCaja(rl, menuPrincipal);
+        }
+        else if(opcion === "3"){
+            calcularTotal();
+            menuCaja(rl, menuPrincipal);
+        }
+        else if(opcion === "4"){
+            notificarPedidoListo(mensaje =>{
+                console.log(mensaje);
+            });
+            menuCaja(rl, menuPrincipal);
+        }
+        else if(opcion === "5"){
+            notificarPedidoCancelado(mensaje => {
+                console.log(mensaje); 
+            });
+            menuCaja(rl, menuPrincipal);
+        }
+        else if(opcion === "6"){
+            menuPrincipal();
+        }
+        else{
+            console.log("opcion no valida intenta un numero del 1 al 6");
+            menuCaja(rl, menuPrincipal);
+
         }
     });
 }
 
-inicializar();
+module.exports = {
+    pedidos,
+    agregarPedido,
+    mostrarPedidos,
+    mostrarTotalAcumulado,
+    calcularTotal,
+    notificarPedidoListo,
+    notificarPedidoCancelado,
+    menuCaja
+
+};
